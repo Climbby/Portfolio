f03d6a4f69d411e61c9f0ef7717bd3a6a6f2d2f5
Climbby <franciscoaf.gaming@gmail.com>
2025-04-26 11:52:14 +0000

    Projetos section revamp

---
+++ index.js
@@ -0,0 +1,98 @@
+const cardsOutline = document.querySelectorAll('.card-outline')
+const seccoes = document.querySelectorAll('#projetos .seccao')
+const titles = document.querySelectorAll('#projetos #titles-container .title')
+const projetos = document.querySelector('.container#projetos')
+let card_id = "portfolio"
+let title_id = "projetos";
+
+function calculate_size(){
+    const projetos__seccao = document.querySelector('#projetos .seccao.projetos')
+    projetos__seccao.style.visibility = 'hidden'
+    projetos__seccao.style.display = 'grid'
+    let maxHeight = projetos__seccao.offsetHeight
+    projetos__seccao.style.visibility = 'visible'
+    if (title_id !== "projetos") projetos__seccao.style.display = 'none'
+
+    // seccoes.forEach(seccao => {
+    //     seccao.style.visibility = 'hidden'
+    //     if (seccao.classList.contains("projetos")){
+    //         seccao.style.display = 'grid'
+    //     }
+    //     maxHeight = Math.max(maxHeight ,seccao.offsetHeight)
+    //     if(seccao.classList.contains("projetos") && title_id !== "projetos"){
+    //         seccao.style.display = 'none'
+    //     }
+    // })
+
+    // seccoes.forEach(seccao => {
+    //     seccao.style.visibility = 'visible'
+    // })
+    
+    projetos.style.paddingBottom = `${maxHeight}px`
+}
+
+// show border based on selected project
+cardsOutline.forEach(outline => {
+    const card = outline.querySelector('.card')
+    card.addEventListener('click', () => {
+
+        card_id = card.id
+        cardsOutline.forEach(outlin => {
+            outlin.classList.remove('active')
+        })
+        outline.classList.add('active')
+  })
+})
+
+// show content based on selected option
+titles.forEach(title => {
+    title.addEventListener('click', () => {
+        
+        title_id = title.id
+        let portfolio_content = document.querySelector(`.seccao.${title_id}`)
+        let content = document.querySelector(`.seccao.${title_id} #${card_id}`)
+
+        // show content based on selected title
+        seccoes.forEach(seccao => {
+
+            if (seccao.classList.contains("projetos")){
+                if (title_id === "projetos") return
+                seccao.style.display = "none"
+                return
+            }
+
+            articles = seccao.querySelectorAll("article")
+            articles.forEach(article => {
+                article.style.display = "none"
+            })
+        })
+
+        switch (title_id) {
+            case "projetos":  
+                portfolio_content.style.display = "grid"
+                break
+                
+            case "visao-geral":
+                content.style.display = "inline-block"
+                break
+                
+            case "demonstracao":
+                content.style.display = "inline-block"
+                break
+
+            case "tecnologias":
+                content.style.display = "inline-block"
+        }   
+
+        // set the title markdown
+        titles.forEach(titulo => {
+            titulo.classList.remove('active')
+            
+        })
+        title.classList.add('active')
+        
+  })
+})
+
+calculate_size()
+window.addEventListener('resize', calculate_size)
