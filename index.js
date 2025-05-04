b006f7cbd38079130c7e196169a7a32026adc615
Climbby <franciscoaf.gaming@gmail.com>
2025-05-04 11:03:17 +0000

    Changed projetos layout

--- index.js
+++ index.js
@@ -6,19 +6,13 @@ const projetos = document.querySelector('.container#projetos')
 const githubs = document.querySelectorAll('.container#projetos .seccao #github')
 const articleLinks = document.querySelectorAll('.container#projetos .seccao.demonstracao article #link')
 let card_id = "portfolio"
-let title_id = "projetos"
-let maxHeight = 0
+let title_id = "visao-geral"
 
 function calculate_size(){
-    const projetos__seccao = document.querySelector('#projetos .seccao.projetos')
-    projetos__seccao.style.visibility = 'hidden'
-    projetos__seccao.style.display = 'grid'
-    maxHeight = projetos__seccao.offsetHeight
-    projetos__seccao.style.visibility = 'visible'
-    if (title_id !== "projetos") projetos__seccao.style.display = 'none'
-    projetos.style.height = `${titlesContainer.offsetHeight + maxHeight}px`
+    const projetos__seccao = document.querySelector('#projetos #the-projetos')
+    projetos.style.height = `${titlesContainer.offsetHeight + projetos__seccao.offsetHeight + 500}px`
     seccoes.forEach(seccao => {
-        seccao.style.height = `${maxHeight}px`
+        seccao.style.height = `${500}px`
     });
     git_pos()
     links_pos()
@@ -27,14 +21,14 @@ function calculate_size(){
 function git_pos(){
     
     githubs.forEach(github => {
-        let zeroHeight = maxHeight - github.offsetHeight
+        let zeroHeight = 500 - github.offsetHeight
         github.style.top = `${zeroHeight - 50}px`
     })
 }
 function links_pos(){
     
     articleLinks.forEach(link => {
-        let zeroHeight = maxHeight - link.offsetHeight
+        let zeroHeight = 500 - link.offsetHeight
         link.style.top = `${zeroHeight - 50}px`
     })
 }
@@ -45,6 +39,8 @@ cardsOutline.forEach(outline => {
     card.addEventListener('click', () => {
 
         card_id = card.id
+        change_content()
+
         cardsOutline.forEach(outlin => {
             outlin.classList.remove('active')
         })
@@ -57,46 +53,7 @@ titles.forEach(title => {
     title.addEventListener('click', () => {
         
         title_id = title.id
-        let portfolio_content = document.querySelector(`.seccao.${title_id}`)
-        let seccao_content = document.querySelector(`.seccao.${title_id}`)
-        let content = document.querySelector(`.seccao.${title_id} #${card_id}`)
-
-        // show content based on selected title
-        seccoes.forEach(seccao => {
-            if (seccao.classList.contains("projetos")){
-                if (title_id === "projetos") return
-                seccao.style.display = "none"
-                return
-            }
-            
-            seccao.style.display = "none"
-            articles = seccao.querySelectorAll("article")
-            articles.forEach(article => {
-                article.style.display = "none"
-            })
-        })
-
-        switch (title_id) {
-            case "projetos":  
-                seccao_content.style.display = "grid"
-                break
-                
-            case "visao-geral":
-                seccao_content.style.display = "block"
-                content.style.display = "inline-block"
-                break
-                
-            case "demonstracao":
-                seccao_content.style.display = "block"
-                content.style.display = "inline-block"
-                git_pos()
-                links_pos()
-                break
-
-            case "tecnologias":
-                seccao_content.style.display = "block"
-                content.style.display = "grid"
-        }   
+        change_content()
 
         // set the title markdown
         titles.forEach(titulo => {
@@ -107,6 +64,40 @@ titles.forEach(title => {
         
   })
 })
+
+function change_content(){
+
+    let seccao_content = document.querySelector(`.seccao.${title_id}`)
+    let content = document.querySelector(`.seccao.${title_id} #${card_id}`)
+
+    // show content based on selected title
+    seccoes.forEach(seccao => {        
+        seccao.style.display = "none"
+        articles = seccao.querySelectorAll("article")
+        articles.forEach(article => {
+            article.style.display = "none"
+        })
+    })
+
+    switch (title_id) {
+        case "visao-geral":
+            seccao_content.style.display = "block"
+            content.style.display = "inline-block"
+            break
+            
+        case "demonstracao":
+            seccao_content.style.display = "block"
+            content.style.display = "inline-block"
+            git_pos()
+            links_pos()
+            break
+
+        case "tecnologias":
+            seccao_content.style.display = "block"
+            content.style.display = "grid"
+    }   
+
+}
 
 calculate_size()
 window.addEventListener('resize', calculate_size)
